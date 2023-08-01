90c09d2cda7a3e8d75f8ef1fd205431794bb067c
Subanesh-swe <subaneshtechie@gmail.com>
2023-08-01 21:21:37 +0530

    modified views in users

--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ def userLogin(request):
         if form.is_valid():
             username = request.POST.get('username')
             password = request.POST.get('password')
-            print("username", username,"password", password)
+            #print("username", username,"password", password)
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
